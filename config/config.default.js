f3f1a23b3e62ed30afd770d4660de754ee5d6467
bearnew <651831513@qq.com>
2019-04-10 23:01:57 +0800

    update

--- config/config.default.js
+++ config/config.default.js
@@ -18,6 +18,15 @@ module.exports = appInfo => {
   // add your middleware config here
   config.middleware = [];
 
+  // port
+  config.cluster = {
+    listen: {
+      path: '',
+      port: 1225,
+      hostname: '127.0.0.1'
+    }
+  }
+
   // add your user config here
   const userConfig = {
     // myAppName: 'egg',
